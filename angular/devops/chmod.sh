8dc8d6ba8e856ffce927d3edeed8ca8c8d287325
Marlon Raphael <raphael27atm@gmail.com>
2023-11-15 16:48:09 -0300

    feat: add angular project

---
+++ angular/devops/chmod.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -e
+
+chmod +x devops/**/*.sh
---
+++ angular/devops/compose/config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+. devops/compose/.env
+
+COMPOSE_FILE=$COMPOSE_FILE \
+ARG_USER_UID=$ARG_USER_UID \
+ARG_USER_GID=$ARG_USER_GID \
+  docker compose config
